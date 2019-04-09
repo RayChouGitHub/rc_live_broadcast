485ff39bd4442e337fe23200655216a04081786e
RayChou <rayzhou2018@gmail.com>
2019-04-09 18:29:01 +0800

    修改部分配置文件

--- rc_live_broadcast_config_client/src/main/java/com/rc/rc_live_broadcast_config_client/RcLiveBroadcastConfigClientApplication.java
+++ rc_live_broadcast_config_client/src/main/java/com/rc/rc_live_broadcast_config_client/RcLiveBroadcastConfigClientApplication.java
@@ -2,8 +2,10 @@ package com.rc.rc_live_broadcast_config_client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class RcLiveBroadcastConfigClientApplication {
 
     public static void main(String[] args) {
